e22bb7347813b23af5a7fabafcd53634c0d098fa
BugFixes <110563077+BuugFixxes@users.noreply.github.com>
2022-11-08 15:16:11 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+/**
+ * @author Lars
+ * @version 0.0.1
+*/
+
+/*    TODO:
+*
+*/
+
+import createfile.templates.*;
+public class Main extends AutoExec {
+    public static void main (String[] args) {
+        final String[] headlineLow = {"\nc","r","e","a","t","e"," a ", "f","i","l","e\n"};
+        final String headlineChar = "-";
+
+        boolean loopExit = false;
+
+        while (!loopExit) {
+            for (String i_OfHeadline : headlineLow) { // for-each loop
+                SleepTimer.sleepTimer();
+                System.out.print(i_OfHeadline.toUpperCase());
+            }
+            for (var i = 0; i < 11; i++) {
+                SleepTimer.sleepTimer();
+                System.out.print(headlineChar);
+            }
+            Create.create();
+            //SearchPattern.searchPattern();
+            Write.write();
+            Read.read();
+        }
+    }
+}
